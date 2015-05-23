45f7accf3b61d08472f8632c57184bd8826d2439
GamaX320 <bryan.x320@gmail.com>
2015-05-23 15:49:00 +0800

    add app bar

--- app/src/main/java/com/example/bryanty/proj_post/TestActivity.java
+++ app/src/main/java/com/example/bryanty/proj_post/TestActivity.java
@@ -1,6 +1,5 @@
 package com.example.bryanty.proj_post;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,25 +7,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-
-public class MainActivity extends ActionBarActivity {
+public class TestActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
 
         //app bar intial
         toolbar= (Toolbar)findViewById(R.id.app_Bar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_test, menu);
         return true;
     }
 
@@ -40,9 +41,6 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
-        else if(id == R.id.action_search){
-            startActivity(new Intent(this, TestActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
